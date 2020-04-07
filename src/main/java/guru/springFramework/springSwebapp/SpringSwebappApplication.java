a601881b404ff829f54068ca091d4b9a158ab144
Itay Peceniski <itay.peck@gmail.com>
2020-04-07 18:05:41 -0300

    first commit

---
+++ src/main/java/guru/springFramework/springSwebapp/SpringSwebappApplication.java
@@ -0,0 +1,13 @@
+package guru.springFramework.springSwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwebappApplication.class, args);
+	}
+
+}
