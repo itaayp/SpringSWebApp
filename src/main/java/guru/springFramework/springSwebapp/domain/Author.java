33c205f914b41106a15f2e37e15230cd93455da6
Itay Peceniski <itay.peck@gmail.com>
2020-04-08 10:51:05 -0300

    I have added the annotations that makes the persistation from my code to the database

--- src/main/java/guru/springFramework/springSwebapp/domain/Author.java
+++ src/main/java/guru/springFramework/springSwebapp/domain/Author.java
@@ -6,14 +6,27 @@
 package guru.springFramework.springSwebapp.domain;
 
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 /**
  *
  * @author Itay
  */
+
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
     private String firstName;
     private String lastName;
+    
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     //Necessary for JPA
@@ -25,7 +38,15 @@ public class Author {
         this.lastName = lastName;
         this.books = books;
     }
+    
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     public String getFirstName() {
         return firstName;
     }
@@ -49,5 +70,31 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+    @Override
+    public String toString() {
+        return "Author{" + 
+                "id=" + id + 
+                ", firstName=" + firstName + 
+                ", lastName=" + lastName + 
+                ", books=" + books + 
+                '}';
+    }
     
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) 
+            return true;
+            
+        if (o == null || getClass() != o.getClass()) 
+            return false;
+        
+        Author author = (Author) o;
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
 }
